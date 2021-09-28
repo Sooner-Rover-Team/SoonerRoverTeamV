77c3bc37bb9a0407fa69d1ac5fafc9f345eae843
Za-Sing <za.sing227@gmail.com>
2021-09-28 17:36:02 -0500

    Working on using argparse and configparser

--- Autonomous/ardetector/ARTracker.py
+++ Autonomous/ardetector/ARTracker.py
@@ -1,30 +1,41 @@
 import cv2 as cv
 import cv2.aruco as aruco
 import argparse
+import configparser
 
 # Read config file and set variables
+
+
 def config():
-    configFile = open("../config.txt")
-    configData = configFile.readlines()
-    #for line in configData:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    args = parser.parse_args()
+    config = configparser.ConfigParser()
+    config.sections()
+    config.read(args.filename)
+    print(config['Config Values']['SWIFT_IP'])
+
 
 
 def arFound(id, image, writeToFile):
     cv.cvtColor(src=image, code=cv.COLOR_RGB2GRAY, dest=image)
     index = -1
 
-    #for i in range(40, 221, 60):
-
-def countValidARs(id1, id2, image, writeToFile):
+    # for i in range(40, 221, 60):
 
 
-def findAR(id):
+#def countValidARs(id1, id2, image, writeToFile):
 
 
-def findARs(id1, id2):
+#def findAR(id):
 
 
-def trackAR(id):
+#def findARs(id1, id2):
 
 
-def trackARs(id1, id2):
+#def trackAR(id):
+
+
+#def trackARs(id1, id2):
+
+config()
