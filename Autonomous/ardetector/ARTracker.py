4fe6d86c23c2b971152ed94483fd7f829e56516a
Za-Sing <za.sing227@gmail.com>
2021-10-03 17:46:44 -0500

    Finished config method

--- Autonomous/ardetector/ARTracker.py
+++ Autonomous/ardetector/ARTracker.py
@@ -1,28 +1,21 @@
 import cv2 as cv
 import cv2.aruco as aruco
-import argparse
 import configparser
 
+
 # Read config file and set variables
-
-
-def config():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filename')
-    args = parser.parse_args()
+def readConfig():
     config = configparser.ConfigParser()
-    config.sections()
-    config.read(args.filename)
-    print(config['Config Values']['SWIFT_IP'])
-
-
+    config.read('../config.ini')
+    configVals = [0, 0, 0]
+    configVals[0] = float(config['CONFIG']['DEGREES_PER_PIXEL'])
+    configVals[1] = float(config['CONFIG']['FOCAL_LENGTH'])
+    configVals[2] = float(config['CONFIG']['KNOWN_TAG_WIDTH'])
+    return configVals
 
 def arFound(id, image, writeToFile):
     cv.cvtColor(src=image, code=cv.COLOR_RGB2GRAY, dest=image)
     index = -1
-
-    # for i in range(40, 221, 60):
-
 
 #def countValidARs(id1, id2, image, writeToFile):
 
@@ -38,4 +31,7 @@ def arFound(id, image, writeToFile):
 
 #def trackARs(id1, id2):
 
-config()
+configVals = readConfig()
+degreesPerPixel = configVals[0]
+focalLength = configVals[1]
+knownTagWidth = configVals[2]
