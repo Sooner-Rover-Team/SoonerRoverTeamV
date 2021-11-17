b1ea5ffb6d7b2566559e1ddf1b2715df8eac5163
Zachary Singleton <za.sing227@gmail.com>
2021-11-17 14:02:42 -0600

    finished arFound()

--- Autonomous/ardetector/ARTracker.py
+++ Autonomous/ardetector/ARTracker.py
@@ -1,8 +1,7 @@
-import cv2 as cv
+import cv2
 import cv2.aruco as aruco
 import numpy as np
 import configparser
-
 
 class ARTracker:
 
@@ -23,38 +22,39 @@ class ARTracker:
         self.frameHeight = float(config['ARTRACKER']['FRAME_HEIGHT'])
         
         # Initialize video writer
-        self.videoWriter = cv.VideoWriter("autonomous.avi", cv.VideoWriter_fourcc(
+        self.videoWriter = cv2.VideoWriter("autonomous.avi", cv2.VideoWriter_fourcc(
             self.format[0], self.format[1], self.format[2], self.format[3]), 5, (1920, 1080), False)
         # Set the ar tag dictionary
         self.tagDict = aruco.DICT_4X4_50
         # Initialize cameras
-        self.caps = np.empty(3, dtype=cv.VideoCapture)
+        self.caps = np.empty(3, dtype=cv2.VideoCapture)
         for i in range(0, self.cameras.size):
-            self.caps[i] = cv.VideoCapture(self.cameras[i])
+            self.caps[i] = cv2.VideoCapture(self.cameras[i])
             if not self.caps[i].isOpened():
                 print(f"Camera  i  did not open!")
-            self.caps[i].set(cv.CAP_PROP_FRAME_WIDTH, self.frameWidth)
-            self.caps[i].set(cv.CAP_PROP_FRAME_HEIGHT, self.frameHeight)
-            self.caps[i].set(cv.CAP_PROP_BUFFERSIZE, 1) # greatly speeds up the program but the writer is a bit wack because of this
-            self.caps[i].set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc(self.format[0], self.format[1], self.format[2], self.format[3]))
+            self.caps[i].set(cv2.CAP_PROP_FRAME_WIDTH, self.frameWidth)
+            self.caps[i].set(cv2.CAP_PROP_FRAME_HEIGHT, self.frameHeight)
+            self.caps[i].set(cv2.CAP_PROP_BUFFERSIZE, 1) # greatly speeds up the program but the writer is a bit wack because of this
+            self.caps[i].set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(self.format[0], self.format[1], self.format[2], self.format[3]))
 
     def arFound(self, id, image, writeToFile):
         # converts to grayscale
-        cv.cvtColor(image, image, cv.COLOR_RGB2GRAY)  
+        cv2.cvtColor(image, image, cv2.COLOR_RGB2GRAY)  
         
         index = -1  
         # tries converting to b&w using different different cutoffs to find the perfect one for the current lighting
         for i in range(40, 221, 60):
             # FIXME corners = [], MarkerIDs = [], rejects = []
             # detects all of the tags in the current b&w cutoff
-            aruco.detectMarkers((image > i), self.tagDict, corners, MarkerIDs, rejects)   
+            #aruco.detectMarkers((image > i), self.tagDict, self.corners, self.markerIDs, self.rejects)
+            (self.corners, self.markerIDs, self.rejected) = aruco.detectMarkers(image, self.tagDict)   
         
-            if MarkerIDs.length > 0:
+            if self.markerIDs.length > 0:
              
                 index = -1 
                 # this just checks to make sure that it found the right tag. Probably should move this into the b&w block
-                for i in range(MarkerIDs.length):  
-                    if MarkerIDs[i] == id:
+                for i in range(self.markerIDs.length):  
+                    if self.markerIDs[i] == id:
                         index = i 
                         break  
             
@@ -68,7 +68,6 @@ class ARTracker:
                 else:
                     print("Found a tag but was not the correct one") 
              
-
             if i == 220:  #did not find any AR tags with any b&w cutoff
              
                 if writeToFile:
@@ -77,17 +76,15 @@ class ARTracker:
                 angleToAR = 0 
                 return False 
              
-         
-        widthOfTag = corners[index][1].x - corners[index][0].x 
+        widthOfTag = self.corners[index][1].x - self.corners[index][0].x 
         distanceToAR = (self.knownTagWidth * self.focalLength) / widthOfTag 
     
-        centerXTag = (corners[index][1].x + corners[index][0].x) / 2 
+        centerXTag = (self.corners[index][1].x + self.corners[index][0].x) / 2 
         # takes the pixels from the tag to the center of the image and multiplies it by the degrees per pixel
         angleToAR = self.degreesPerPixel * (centerXTag - 960)   
     
         return True 
          
-
 
     # def countValidARs(self, id1, id2, image, writeToFile):
 
