7e86f4bd6a675438f289d7e8efc4427a7533bf9e
yukijx <yukizhengjx@gmail.com>
2021-12-29 14:35:47 -0600

    updated code for user commandline arguments, will need to add AR Tag detection code with Tyler later

--- Autonomous/YOLO/imageCollector.py
+++ Autonomous/YOLO/imageCollector.py
@@ -3,6 +3,7 @@ import os
 import argparse
 import time
 
+# creates and initializes the parser (allows custom arguments)
 parser = argparse.ArgumentParser(description='save images to file')
 
 # add the arguments NEED TO WORK ON THESE
@@ -18,7 +19,10 @@ parser.add_argument('-captureRate', help='frame capture rate specifies collectio
 # set iteration to stop after 200 frames has been collected
 parser.add_argument('-iterationLength', help='the duration of one iteration', type=int, default= 200)
 
-# Execute the parse_args() method
+# arMode - user decides through command prompt if they want code to run through arMode
+parser.add_argument('-arMode', help='arMode identifies the arTags in captured frames, argument is "run"', type=str, default='.')
+
+# Execute the parse_args() method (parse the argument)
 args = parser.parse_args()
 
 # Creates camera object
@@ -32,37 +36,77 @@ numFramesCollected = 0
 
 while True:
 
-    # reading from frame
-    ret, frame = cam.read()
+    # if the user specifies to run code through arMode
+    if args.arMode == "run": 
 
-    # updates the window "preview" to show user captured frames 
-    cv.imshow('preview', frame)
+        # testing block 
+        # reading from frame
+        ret, frame = cam.read()
 
-    # incrementing count 
-    counter += 1
+        # updates the window "preview" to show user captured frames 
+        cv.imshow('preview', frame)
 
-    # if count equals captureRate, save image 
-    if counter == args.captureRate:
+        # incrementing count 
+        counter += 1
 
-        # name filename based on timestamp 
-        filename = args.file + '/' + str(time.time()) + '.jpg'
-        cv.imwrite(filename, frame)
+        # if count equals captureRate, save image 
+        if counter == args.captureRate:
 
-        # create .txt file with the same filename 
-        # open("filename.txt", "w") -- accessmode "w" indicates python will write file and create 
-        txtfilename = open(args.file + '/' + str(time.time()) + '.txt', "w")
-        txtfilename.write("")
-        txtfilename.close
+            # name filename based on timestamp
+            filename = args.file + '/' + str(time.time()) + '.jpg'
+            cv.imwrite(filename, frame)
+        
+            # create .txt file with the same filename 
+            # open("filename.txt", "w") -- accessmode "w" indicates python will write and create the new file 
+            txtfilename = open(args.file + '/' + str(time.time()) + '.txt', "w")
+            txtfilename.write("")
+            txtfilename.close
 
-        # increment the number of frames collected 
-        numFramesCollected += 1
+            # increment the number of frames collected 
+            numFramesCollected += 1
 
-        #reset counter
-        counter = 0
+            #reset counter
+            counter = 0
 
-    # quit if user presses 'q' or when 200 frames has been collected 
-    if cv.waitKey(1) == ord('q') or numFramesCollected == args.iterationLength:
-        break
+        # quit if user presses 'q' or when 200 frames has been collected 
+        if cv.waitKey(1) == ord('q') or numFramesCollected == args.iterationLength:
+            break
+
+
+
+    # if the user chooses not to run code through arMode 
+    else: 
+        # reading from frame
+        ret, frame = cam.read()
+
+        # updates the window "preview" to show user captured frames 
+        cv.imshow('preview', frame)
+
+        # incrementing count 
+        counter += 1
+
+        # if count equals captureRate, save image 
+        if counter == args.captureRate:
+
+            # name filename based on timestamp
+            filename = args.file + '/' + str(time.time()) + '.jpg'
+            cv.imwrite(filename, frame)
+
+            # create .txt file with the same filename 
+            # open("filename.txt", "w") -- accessmode "w" indicates python will write and create the new file 
+            txtfilename = open(args.file + '/' + str(time.time()) + '.txt', "w")
+            txtfilename.write("")
+            txtfilename.close
+
+            # increment the number of frames collected 
+            numFramesCollected += 1
+
+            #reset counter
+            counter = 0
+
+        # quit if user presses 'q' or when 200 frames has been collected 
+        if cv.waitKey(1) == ord('q') or numFramesCollected == args.iterationLength:
+            break
 
 cam.release()
 cv.destroyAllWindow()
