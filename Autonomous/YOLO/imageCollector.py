08c297bdbbd0c9897357afa42a1d6846e6d37e19
RezaTorbati <kofcrotmgiv@gmail.com>
2022-01-09 14:22:37 -0600

    Modfied the code to be a bit cleaner and started the process to take in an array of indexes to check for

--- Autonomous/YOLO/imageCollector.py
+++ Autonomous/YOLO/imageCollector.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import argparse
 import time
+import sys
 
 # Set the ar tag dictionary
 tagDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
@@ -11,27 +12,20 @@ tagDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
 # creates and initializes the parser (allows custom arguments)
 parser = argparse.ArgumentParser(description='save images to file')
 
-# add the arguments
-# camera
 parser.add_argument('-cam', help='camera path', type=int, default=0)
-
-# filepath
 parser.add_argument('-file', help='File path where images will be stored', default='.')
-
-# set frame capture rate to every 5 frames
-parser.add_argument('-captureRate', help='frame capture rate specifies collection frequency', type=int, default='5')
-
-# set iteration to stop after 200 frames has been collected
+parser.add_argument('-captureRate', help='frame capture rate specifies collection frequency', type=int, default='10') # Changed default from 5 to 10
 parser.add_argument('-iterationLength', help='the duration of one iteration', type=int, default= 200)
-
-# arMode - user decides through command prompt if they want code to run through arMode 
-parser.add_argument('-arMode', help='arMode identifies the arTags in captured frames, argument is "run"', type=bool, default=False)
-
-# Execute the parse_args() method (parse the argument) 
+parser.add_argument('-arMode', help='arMode identifies the arTags in captured frames, argument is "run"', action='count', default=0)
+parser.add_argument('-tagIDs', nargs='+', type=int, default=[0], help='Array of tag ids') 
+# Parse the arguments
 args = parser.parse_args()
 
 # Creates camera object 
 cam = cv.VideoCapture(args.cam)
+if not cam.isOpened():
+    print("!!!!!CAMERA DID NOT OPEN!!!!!")
+    sys.exit()
 
 # define counter (this counter determines the rate at which frames are collected, ie every 5 frames)
 counter = 0
@@ -44,7 +38,6 @@ while True:
     # if the user specifies to run code through arMode 
     if args.arMode: 
         
-        # testing block 
         # reading from frame 
         ret, frame = cam.read() 
 
@@ -62,60 +55,58 @@ while True:
 
             # name filename based on timestamp and add it to specified filepath 
             filename = args.file + '/' + fileTime + '.jpg'
-            # assigning the converted grayscale image to the filename
-            
 
-            # Add note 
             for i in range(40, 221, 60): 
                 bwFrame = cv.threshold(frame, i, 255, cv.THRESH_BINARY)[1]
+
                 # corners: x,y coordinates of our detected markers
                 # markerIDs: identifers of the marker (the ID encoded in the marker itself)
                 # rejected: potential markers detected but rejected bc code inside marker couldn't be parsed 
                 # bwFrame: frame converted to grayscale 
                 # tagDict: tag dictionary we previously set up 
                 (corners, markerIDs, rejected) = cv2.aruco.detectMarkers(bwFrame, tagDict)
-
+                print(markerIDs)
                 # determining the height and width of the frame (NOT THE TAG) 
                 height, width = bwFrame.shape[0], bwFrame.shape[1]
-                # print("h" + str(h) + ", w" + str(w))      
                 
                 # if any markerIDs were detected in the frame 
                 # basically if markerIDs > 0 
                 if not (markerIDs is None) : 
                     cv.imwrite(filename, frame)
-                    # X and Y coordinates: 
-                    # determined by finding the midpoint of x and y (i.e. ((x1 + x2)/2)) and dividing by image width or height
-                    # Notes: X_CENTER_NORM = X_CENTER_ABS/IMAGE_WIDTH
-                    # Notes: Y_CENTER_NORM = Y_CENTER_ABS/IMAGE_HEIGHT
-                    xTag = ((corners[0][0][1][0] + corners[0][0][0][0] + corners[0][0][2][0] + corners[0][0][3][0]) / 4 ) / width
-                    yTag = ((corners[0][0][1][1] + corners[0][0][2][1]) / 2 ) / height
+                    #markerIDs = markerIDs.flatten()
+                    for i in range(0, len(markerIDs)):
+                        print(i)
+                        if markerIDs[i][0] <= 7:
+                            # X and Y coordinates: 
+                            # determined by finding the midpoint of x and y (i.e. ((x1 + x2)/2)) and dividing by image width or height
+                            # Notes: X_CENTER_NORM = X_CENTER_ABS/IMAGE_WIDTH
+                            # Notes: Y_CENTER_NORM = Y_CENTER_ABS/IMAGE_HEIGHT
+                            xTag = ((corners[i][0][1][0] + corners[i][0][0][0] + corners[i][0][2][0] + corners[i][0][3][0]) / 4 ) / width
+                            yTag = ((corners[i][0][1][1] + corners[i][0][2][1]) / 2 ) / height
 
-                    # Width and Height of Tag: 
-                    # Notes: WIDTH_NORM = WIDTH_OF_LABEL_ABS/IMAGE_WIDTH
-                    # Notes: HEIGHT_NORM = HEIGHT_OF_LABEL_ABS/IMAGE_HEIGHT
-                    widthOfTag = ((corners[0][0][1][0] - corners[0][0][0][0]) + corners[0][0][2][0] - corners[0][0][3][0]) / (2*width)
-                    heightOfTag = (corners[0][0][2][1] - corners[0][0][1][1]) / height
+                            # Width and Height of Tag: 
+                            # Notes: WIDTH_NORM = WIDTH_OF_LABEL_ABS/IMAGE_WIDTH
+                            # Notes: HEIGHT_NORM = HEIGHT_OF_LABEL_ABS/IMAGE_HEIGHT
+                            widthOfTag = (((corners[i][0][1][0] - corners[i][0][0][0]) + (corners[i][0][2][0] - corners[i][0][3][0])) / 2)  / width
+                            heightOfTag = (corners[i][0][2][1] - corners[i][0][1][1]) / height
 
-                    # converting centerXTag and centerYTag from numpy long to str (if not, will produce error)
-                    print(f'(x: {xTag}, y: {yTag}, width: {widthOfTag}, height: {heightOfTag}')
-                    #print("(" + str(xTag) + ", " + str(yTag) + ")")
-
-                    # create .txt file with the same filename 
-                    # open("filename.txt", "w") -- accessmode "w" indicates python will write and create the new file 
-                    txtfilename = open(args.file + '/' + fileTime + '.txt', "w") 
-                    # write in the txt file: 0  X_CENTER_NORM  Y_CENTER_NORM  WIDTH_NORM  HEIGHT_NORM 
-                    # we are writing in the txt file since there was an AR Tag found 
-                    txtfilename.write("0 " + str(xTag) + " " + str(yTag)  + " " + str(widthOfTag) + " " + str(heightOfTag))
-                    txtfilename.close()
+                            # create .txt file with the same filename 
+                            # open("filename.txt", "w") -- accessmode "w" indicates python will write and create the new file 
+                            txtfilename = open(args.file + '/' + fileTime + '.txt', "a") 
+                            # write in the txt file: 0  X_CENTER_NORM  Y_CENTER_NORM  WIDTH_NORM  HEIGHT_NORM 
+                            txtfilename.write("0 " + str(xTag) + " " + str(yTag)  + " " + str(widthOfTag) + " " + str(heightOfTag) + "\n")
+                            txtfilename.close()
 
                 # AR Tag is not found 
                 else: 
                     # create .txt file with the same filename 
+                    print("sucks to suck")
+                    #pass
                     # open("filename.txt", "w") -- accessmode "w" indicates python will write and create the new file 
-                    txtfilename = open(args.file + '/' + fileTime + '.txt', "w")
+                    # txtfilename = open(args.file + '/' + fileTime + '.txt', "w")
                     # file will be empty if AR Tag is not found 
-                    txtfilename.write("")
-                    txtfilename.close
+                    # txtfilename.write("")
+                    # txtfilename.close
 
             # increment the number of frames collected 
             numFramesCollected += 1
