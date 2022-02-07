11877c12b845805d091b13397c1f98492855c3a8
Tyler <tyler73750@gmail.com>
2022-02-06 21:41:57 -0600

    made function in location.py that takes bearing and distance but returns coordinates, and made comments to clarify functionality.

--- Autonomous/libs/Location.py
+++ Autonomous/libs/Location.py
@@ -1,10 +1,11 @@
 import sys
 sys.path.append('../')
 from gps import gps
-from math import cos, sin, atan2, pi, sqrt
+from math import cos, radians, degrees, sin, atan2, pi, sqrt, asin
 from threading import Thread
 from time import sleep
 
+# Class that computes functions related to location of Rover
 class Location:
     def __init__(self, ip, port):
         self.swift_IP = ip
@@ -26,6 +27,7 @@ class Location:
         # ConfigParser because that's ez
         pass
 
+    # Returns distance in kilometers between given latitude and longitude
     def distance_to(self, lat:float, lon:float):
         earth_radius = 6371.301
         delta_lat = (lat - self.latitude) * (pi/180.0)
@@ -35,10 +37,13 @@ class Location:
         c = 2 * atan2(sqrt(a), sqrt(1-a))
         return earth_radius * c
 
+    # Calculates difference between given bearing to location and current bearing
+    # Positive is turn right, negative is turn left
     def bearing_to(self, lat:float, lon:float):
         resultbearing = self.calc_bearing(self.latitude, self.longitude, lat, lon)
         return resultbearing + 360 if resultbearing < -180 else (resultbearing - 360 if resultbearing > 180 else resultbearing)
 
+    # Starts updating fields from the GPS box
     def start_GPS_thread(self):
         self.running = True
         t = Thread(target=self.update_fields_loop, name=(
@@ -46,6 +51,7 @@ class Location:
         t.daemon = True
         t.start()
 
+    # Stops updating fields from GPS box
     def stop_GPS_thread(self):
         self.running = False
 
@@ -57,6 +63,7 @@ class Location:
         self.running = False
         gps.gps_finish()        
     
+
     def update_fields_loop(self):
         while(self.running):
             if gps.get_latitude() + gps.get_longitude() != 0:
@@ -76,7 +83,26 @@ class Location:
             sleep(self.wait_time)
         return
 
+    # Calculates bearing between two points. 
+    # (0 is North, 90 is East, +/-180 is South, -90 is West)
     def calc_bearing(self,lat1:float, lon1:float, lat2:float, lon2:float):
         x = cos(lat2 * (pi/180.0)) * sin((lon2-lon1) * (pi/180.0))
         y = cos(lat1 * (pi/180.0)) * sin(lat2 * (pi/180.0)) - sin(lat1 * (pi/180.0)) * cos(lat2 * (pi/180.0)) * cos((lon2-lon1) * (pi/180.0))
         return (180.0/pi) * atan2(x,y)
+
+    # Calculate latitutde and longitude given distance (in km) and bearing (in degrees)
+    def distance_to_given_bearing(self, distance:float, bearing:float):
+        # https://stackoverflow.com/questions/7222382/get-lat-long-given-current-point-distance-and-bearing
+        R = 6371.301
+        brng = radians(bearing)      # Assuming bearing is in radians
+        d = distance
+
+        lat1 = radians(self.latitude)   # Current lat point converted to radians
+        lon1 = radians(self.longitude)  # Current long point converted to radians
+
+        lat2 = asin(sin(lat1)*cos(d/R) + 
+                    cos(lat1)-sin(lat1)*cos(brng))
+        lon2 = lon1 + atan2(sin(brng)*sin(d/R)*cos(lat1),
+                            cos(d/R)-sin(lat1)*sin(lat2))
+
+        return degrees(lat2), degrees(lon2)
