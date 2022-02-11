a75becfb44533bc8c6a7bccaf9e6e2c6f6019eda
bentonsmith8 <bentonsmith@ou.edu>
2022-02-10 19:47:18 -0600

    updated MBED code

--- Mission Control/python_drive/manual_drive.py
+++ Mission Control/python_drive/manual_drive.py
@@ -3,6 +3,8 @@ import socket
 import configparser
 from math import ceil, floor
 from pygame.time import Clock
+import os
+from inspect import getsourcefile
 
 pygame.joystick.init()
 
@@ -11,14 +13,34 @@ joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_coun
 for joystick in joysticks:
     print('Found',joystick.get_name())
 
+def find_config():
+    current_folder = os.getcwd().split('/')[-1]
+    if current_folder == 'SoonerRoverTeamV':
+        os.chdir(os.getcwd() + '/Mission Control/python_drive/')
+
+find_config()
 
 timer = Clock()
 THRESHOLD = 0.08
-FPS = 20
+FPS = 30
+
 config = configparser.ConfigParser()
-config.read('config.ini')
-HOST = config['DEFAULT']['HOST']
-PORT = int(config['DEFAULT']['PORT'])
+config.read('./config.ini')
+HOST = config['Connection']['HOST']
+PORT = int(config['Connection']['PORT'])
+CONT_TYPE = config['Controller']['TYPE']
+
+if CONT_TYPE == 'XboxSeriesX':
+    L_X_AXIS = 0
+    L_Y_AXIS = 1
+    R_X_AXIS = 2
+    R_Y_AXIS = 3
+
+elif CONT_TYPE == 'Xbox360':
+    L_X_AXIS = 0
+    L_Y_AXIS = 1
+    R_X_AXIS = 3
+    R_Y_AXIS = 4
 
 pygame.init()
 
@@ -40,8 +62,18 @@ def make_message(leftwheels, rightwheels):
     msg[8] = sum(msg[2:8]) & 0xff
     return msg
 
+def isstopped(leftwheels,rightwheels):
+    for s in leftwheels:
+        if s != 126:
+            return 0
+    for s in rightwheels:
+        if s != 126:
+            return 0
+    return 1
+
 if __name__ == "__main__":
     running = True
+    stopsent = False
     leftwheels = [127] * 3
     rightwheels = [127] * 3
     while(running):
@@ -54,21 +86,25 @@ if __name__ == "__main__":
         for joystick in joysticks:
             if joystick.get_button(6) and joystick.get_button(7):
                 running = False
-            L_X = joystick.get_axis(0)
-            L_Y = joystick.get_axis(1)
-            R_X = joystick.get_axis(2)
-            R_Y = joystick.get_axis(3)
+            L_X = joystick.get_axis(L_X_AXIS)
+            L_Y = joystick.get_axis(L_Y_AXIS)
+            R_X = joystick.get_axis(R_X_AXIS)
+            R_Y = joystick.get_axis(R_Y_AXIS)
             # print('fixed',axistospeed(L_Y))
             if abs(L_Y) > THRESHOLD:
                 leftwheels = [axistospeed(L_Y)] * 3
-                print('leftwheels',leftwheels)
+            else:
+                leftwheels = [126] * 3
+            # print('leftwheels',leftwheels)
                 # print('L_Y',L_Y)
             if abs(L_X) > THRESHOLD:
                 # print('L_X',L_X)
                 pass
             if abs(R_Y) > THRESHOLD:
                 rightwheels = [axistospeed(R_Y)] * 3
-                print('rightwheels',rightwheels)
+            else:
+                rightwheels = [126] * 3
+            # print('rightwheels',rightwheels)
                 # print('R_Y',R_Y)
             if abs(R_X) > THRESHOLD:
                 # print('R_X',R_X)
@@ -77,7 +113,15 @@ if __name__ == "__main__":
 
         data = make_message(leftwheels, rightwheels)
         # print(data)
-        s.sendall(data)
+        if (isstopped(leftwheels,rightwheels)):
+            if not stopsent:
+                print('sent',data[2:8])
+                s.sendall(data)
+                stopsent = True
+        else:
+            print('sent',data[2:8])
+            s.sendall(data)
+            stopsent = False
 
         timer.tick(FPS)
 
