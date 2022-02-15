f17d899fbcd1326564daf21ae4a5e3e4d74c1c86
bentonsmith8 <bentonsmith@ou.edu>
2022-02-14 18:53:15 -0600

    updated MBED code

--- MBED/EBoxMBED/main.cpp
+++ MBED/EBoxMBED/main.cpp
@@ -5,9 +5,14 @@
 // Import from https://os.mbed.com/users/tecnosys/code/mcp2515/
 #include "CAN3.h"
 
+// byte to signify start of communication
+// can be any byte it does not matter
 #define startbyte 0x23
+// port to listen on
 #define PORT 80
-#define DEBUG 1
+// debug and debug printing, disable when
+// actually using
+#define DEBUG 0
 #define dbprint(fmt, ...) do {if (DEBUG) printf(fmt, __VA_ARGS__); } while (0);
 
 // Network interface
@@ -16,12 +21,27 @@ EthernetInterface net;
 // SPI definition
 SPI spi(p11,p12,p13);
 
+// LEDS
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+
+// Timeout for blinking and reseting wheels (not implemented yet)
+Timeout blink, wheelreset;
+
 // Chip Select
 DigitalOut cs1(p14);
+DigitalOut relay(p16);
 
 // CAN interface
 CAN3 can(spi, p14, p15);
 
+// callback
+void LEDOff() {
+    led2 = 0;
+}
+
+// send the wheel speeds over CAN
 void sendWheelSpeeds(char *speeds, char cs) {
     speeds[7] = cs;
     CANMessage c_msg(1, speeds, 8, CANData, CANStandard);
@@ -29,9 +49,28 @@ void sendWheelSpeeds(char *speeds, char cs) {
     dbprint("data has been written\n",0);
 }
 
-// Socket demo
+// check if the message is stop
+int isStop(char *speeds) {
+    for (int i = 0; i < 6; i++) {
+        if (speeds[i] != 126)
+            return 0;
+    }
+    return 1;
+}
+
+// turn the wheels on and off, not implemented yet
+void toggleWheels() {
+    relay = !relay;
+    led3 = !led3;
+}
+
+
 int main()
 {
+    led1 = 0;
+    relay = 0;
+    led2 = 0;
+    led3 = 1;
     can.frequency(8000000);
     // Bring up the ethernet interface
     dbprint("Setting up UDP Socket over ethernet\n",0);
@@ -40,16 +79,26 @@ int main()
     // Show the network address
     SocketAddress a;
     net.get_ip_address(&a);
+    while(!a.get_ip_address()) {
+        net.get_ip_address(&a);
+    }
     dbprint("IP address: %s\n", a.get_ip_address() ? a.get_ip_address() : "None");
+    if (a.get_ip_address())
+        led1 = 1;
+    
 
     // Open a socket on the network interface
     UDPSocket socket;
     socket.open(&net);
     // Listen on port 80
     socket.bind(PORT);
+    
+    // create variables for holding datas
     char data[9];
     char speeds[6];
+    char stopmsg = 0;
     while (1) {
+        // receive the data
         int size = socket.recv(data,PORT);
         if (DEBUG) {
             printf("data: %s\n",data);
@@ -58,6 +107,7 @@ int main()
             }
             printf("\n");
         }
+        // if the message start is correct
         if (data[0] == startbyte) {
             dbprint("Start of message\n",0);
             if (data[1] == 0x00) {
@@ -68,16 +118,35 @@ int main()
                     cs += data[i];
                     speeds[i-2] = data[i];
                 }
+                // checksum
                 cs &= 0xff;
                 if (data[8] == cs) {
                     dbprint("checksum verified: %x\n",cs);
-                    sendWheelSpeeds(speeds,cs);
-                    dbprint("wheel speeds sent\n",0);
+                    if (isStop(speeds)) {
+                        if (!stopmsg) {
+                            sendWheelSpeeds(speeds,cs);
+                            stopmsg = 1;
+                            led2 = 1;
+                            dbprint("wheel speeds sent\n",0);
+                        }
+                    } else {
+                        sendWheelSpeeds(speeds,cs);
+                        stopmsg = 0;
+                        led2 = 1;
+                        dbprint("wheel speeds sent\n",0);
+                    }
+                    blink.attach(&LEDOff, .2f);
+
                 }
                 else
                     dbprint("checksum failed, cs: %d, data[8]: %d\n",cs,data[8]);
             }
+            else if (data[1] = 0x01) {
+                dbprint("message to toggle the wheels\n",0);
+                toggleWheels();
+            }
         }
+        // set data to 0
         memset(data, 0, sizeof(data));
     }
 
--- MBED/WheelMBED/main.cpp
+++ MBED/WheelMBED/main.cpp
@@ -6,10 +6,13 @@
 // imported from https://os.mbed.com/users/simon/code/Servo/
 #include "Servo.h"
 
-#define DEBUG 1
+// debug print, turn this off because it's slow
+#define DEBUG 0
 #define dbprint(fmt, ...) do {if (DEBUG) printf(fmt, __VA_ARGS__); } while (0);
 
-#define AUTOSTOP 5.0
+// autostop is one second
+#define AUTOSTOP 1.0
+// hard code the wheel number to the MBED
 #define WHEEL_NUM 0
 
 // SPI for CAN object
@@ -22,44 +25,66 @@ CAN3 can(spi, A3, A2);
 Servo motor(D9);
 
 // Timeout for automatically stopping the motors after a specified time
-Timeout autostop;
+Timeout autostop, blink;
+
+// LED
+DigitalOut led(D13);
 
 // Function to stop motor
 void stopWheel() {
     motor = .5f;
-    dbprint("5 seconds have passed, stopping wheel\r\n",0);
+}
+
+// blink callback
+void blinkLED() {
+    led = 0;
 }
 
 int main() {
     // 8 MHz frequency
     can.frequency(8000000);
+    // set motor to off
     motor = .5f;
     float speeds[6];
+    // CAN message struct
     CANMessage c_msg;
     wait(.1);
+    // discard the first two can messages read, i don't know
+    // why but this is necessary if you don't want the wheels to
+    // start moving in random directions
     can.read(&c_msg);
     can.read(&c_msg);
     while(1) {
+        // if theres no message do nothing
         if (can.checkReceive() == CAN_NOMSG) continue;
+        // blink LED if there is a message
+        led = 1;
+        blink.attach(&blinkLED,.1f);
+        // read the CAN message
         can.read(&c_msg);
         int sum = 0;
+        // calculate checksum and convert to motor format
         for (size_t i = 0; i < 6; i++) {
             char speed = c_msg.data[i];
             sum += speed;
             speeds[i] = speed / 252.0;
-            dbprint("wheel %d speed: %d, hex: %x\r\n",i+1,speed,speed);
+//            dbprint("wheel %d speed: %d, hex: %x\r\n",i+1,speed,speed);
         }
+//        dbprint("wheel %d speed: %d\r\n",WHEEL_NUM,speeds[WHEEL_NUM]);
         sum &= 0xff;
-        if (sum == c_msg.data[7]) {
-            dbprint("checksum verified: %x\r\n",sum);
-        } else {
-            dbprint("checksum failed: sum: %x, data: %x\r\n",sum,c_msg.data[7]);
-        }
-        
+        // commented out because the code literally would not compile
+        // because of size
+//        if (sum == //c_msg.data[7]) {
+//            dbprint("checksum verified: %x\r\n",sum);
+//        } else {
+//            dbprint("checksum failed: sum: %x, data: %x\r\n",sum,c_msg.data[7]);
+//        }
+        // set motor speed if it is different
         if (motor != speeds[WHEEL_NUM]) {
             dbprint("setting wheel speed to %f\r\n",speeds[WHEEL_NUM]*2-1);
             motor = speeds[WHEEL_NUM];
         }
+        // stop the wheel after 1 second
         autostop.attach(&stopWheel,AUTOSTOP);
     }
 }
