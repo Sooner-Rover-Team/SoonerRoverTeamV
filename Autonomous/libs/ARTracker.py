8f36436d01cadae74a9f039b5473790f7e56f65c
Barrett Ray <onkoe@protonmail.com>
2023-06-15 10:03:32 -0500

    fix: Add correct deps for ARTracker

--- Autonomous/libs/ARTracker.py
+++ Autonomous/libs/ARTracker.py
@@ -1,11 +1,11 @@
-import cv2
-import cv2.aruco as aruco
-import configparser
+from cv2 import aruco, cv2
+from configparser import ConfigParser
 import sys
 import os
 import darknet
 from YOLO.darknet.darknet_images import simple_detection, complex_detection
 from darknet import load_network
+from typing import List
 
 # FIXME: why is this added to path wtf
 darknetPath: str = os.path.dirname(os.path.abspath(__file__)) + "/../YOLO/darknet/"
@@ -19,44 +19,52 @@ class ARTracker:
     """
 
     # Constructor
-    def __init__(self, cameras, write=False, useYOLO=False, configFile="config.ini"):
+    def __init__(
+        self,
+        camera_list: List[str],
+        write_to_disk: bool = False,
+        use_YOLO_tracking: bool = False,
+        config_file_path: str = "config.ini",
+    ):
         """_summary_
         Cameras should be a list of file paths to cameras that are to be used.
         Set write to True to write to disk what the cameras are seeing.
         Set useYOLO to True to use yolo when attempting to detect the ar tags.
         """
 
-        self.write = write
-        self.distanceToMarker = -1
-        self.angleToMarker = -999.9
-        self.index1 = -1
-        self.index2 = -1
-        self.useYOLO = useYOLO
-        self.cameras = cameras
+        self.write_to_disk: bool = write_to_disk
+        self.distanceToMarker: float = -1
+        self.angleToMarker: float = -999.9  # i feel this is not a good default ~bray
+        self.index1 = -1  # the ARUCO tag to track
+        self.index2 = -1  # (optional) second ARUCO tag used for gates (two qr sticks)
+        self.use_YOLO_tracking = use_YOLO_tracking
+        # list of paths to cameras (i.e. /dev/video0)
+        self.camera_list: List[str] = camera_list
 
         # Open the config file
-        config = configparser.ConfigParser(allow_no_value=True)
-        if not config.read(configFile):
+        config = ConfigParser(allow_no_value=True)
+        if not config.read(config_file_path):
             print("ERROR OPENING AR CONFIG:", end="")
-            if os.path.isabs(configFile):
-                print(configFile)
+            if os.path.isabs(config_file_path):
+                print(config_file_path)
             else:
-                print("{os.getcwd()}/{configFile}")
+                print("{os.getcwd()}/{config_file_path}")
             exit(-2)
 
         # Set variables from the config file
-        self.degreesPerPixel = float(config["ARTRACKER"]["DEGREES_PER_PIXEL"])
-        self.vDegreesPerPixel = float(config["ARTRACKER"]["VDEGREES_PER_PIXEL"])
-        self.focalLength = float(config["ARTRACKER"]["FOCAL_LENGTH"])
+        # TODO: parse to data structure instead (serde-like?)
+        self.degrees_per_pixel = float(config["ARTRACKER"]["DEGREES_PER_PIXEL"])
+        self.vdegrees_per_pixel = float(config["ARTRACKER"]["VDEGREES_PER_PIXEL"])
+        self.focal_length = float(config["ARTRACKER"]["FOCAL_LENGTH"])
         self.focalLength30H = float(config["ARTRACKER"]["FOCAL_LENGTH30H"])
         self.focalLength30V = float(config["ARTRACKER"]["FOCAL_LENGTH30V"])
         self.knownMarkerWidth = float(config["ARTRACKER"]["KNOWN_TAG_WIDTH"])
-        self.format = config["ARTRACKER"]["FORMAT"]
+        self.format: str = config["ARTRACKER"]["FORMAT"]
         self.frameWidth = int(config["ARTRACKER"]["FRAME_WIDTH"])
         self.frameHeight = int(config["ARTRACKER"]["FRAME_HEIGHT"])
 
-        # sets up yolo
-        if useYOLO:
+        # Set up YOLO tracking (bounds detection)
+        if use_YOLO_tracking:
             os.chdir(darknetPath)
             weights = config["YOLO"]["WEIGHTS"]
             cfg = config["YOLO"]["CFG"]
@@ -71,7 +79,7 @@ class ARTracker:
             self.networkHeight = darknet.network_height(self.network)
 
         # Initialize video writer, fps is set to 5
-        if self.write:
+        if self.write_to_disk:
             self.videoWriter = cv2.VideoWriter(
                 "autonomous.avi",
                 cv2.VideoWriter_fourcc(
@@ -87,12 +95,12 @@ class ARTracker:
 
         # Initialize cameras
         self.caps = []
-        if isinstance(self.cameras, int):
-            self.cameras = [self.cameras]
-        for i in range(0, len(self.cameras)):
+        if isinstance(self.camera_list, int):
+            self.camera_list = [self.camera_list]
+        for i in range(0, len(self.camera_list)):
             # Makes sure the camera actually connects
             while True:
-                cam = cv2.VideoCapture(self.cameras[i])
+                cam = cv2.VideoCapture(self.camera_list[i])
                 if not cam.isOpened():
                     print(
                         f"!!!!!!!!!!!!!!!!!!!!!!!!!! \
@@ -164,9 +172,9 @@ class ARTracker:
                 bw, self.markerDict
             )
             if self.markerIDs is not None:
-                print(
-                    "", end=""
-                )  # I have not been able to reproduce an error when I have a print statement here so I'm leaving it in
+                # I have not been able to reproduce an error when I have a
+                # print statement here so I'm leaving it in
+                print("", end="")
                 if id2 == -1:  # single post
                     self.index1 = -1
                     # this just checks to make sure that it found the right marker
@@ -177,7 +185,7 @@ class ARTracker:
 
                     if self.index1 != -1:
                         print("Found the correct marker!")
-                        if self.write:
+                        if self.write_to_disk:
                             self.videoWriter.write(bw)  # purely for debug
                             cv2.waitKey(1)
                         break
@@ -200,17 +208,17 @@ class ARTracker:
                                 self.index2 = j
                     if self.index1 != -1 and self.index2 != -1:
                         print("Found both markers!")
-                        if self.write:
+                        if self.write_to_disk:
                             self.videoWriter.write(bw)  # purely for debug
                             cv2.waitKey(1)
                         break
 
             if i == 220:  # did not find any AR markers with any b&w cutoff using aruco
                 # Checks to see if yolo can find a tag
-                if self.useYOLO:
+                if self.use_YOLO_tracking:
                     detections = []
-                    if not self.write:
-                        # this is a simpler detection function that doesn't return the image
+                    if not self.write_to_disk:
+                        # this is a simpler detection fn that doesn't return the image
                         detections = simple_detection(
                             image, self.network, self.class_names, self.thresh
                         )
@@ -230,21 +238,21 @@ class ARTracker:
                     if id2 == -1 and len(detections) > 0:
                         self.corners = self._convertToCorners(detections, 1)
                         self.index1 = 0  # Takes the highest confidence ar tag
-                        if self.write:
+                        if self.write_to_disk:
                             self.videoWriter.write(image)  # purely for debug
                             cv2.waitKey(1)
                     elif len(detections) > 1:
                         self.corners = self._convertToCorners(detections, 2)
                         self.index1 = 0  # takes the two highest confidence ar tags
                         self.index2 = 1
-                        if self.write:
+                        if self.write_to_disk:
                             self.videoWriter.write(image)  # purely for debug
                             cv2.waitKey(1)
                     print(self.corners)
 
                 # Not even YOLO saw anything
                 if self.index1 == -1 or (self.index2 == -1 and id2 != -1):
-                    if self.write:
+                    if self.write_to_disk:
                         self.videoWriter.write(image)
                         # cv2.imshow('window', image)
                         cv2.waitKey(1)
@@ -259,21 +267,28 @@ class ARTracker:
                 + self.corners[self.index1][0][2][0]
                 + self.corners[self.index1][0][3][0]
             ) / 4
-            # takes the pixels from the marker to the center of the image and multiplies it by the degrees per pixel
-            self.angleToMarker = self.degreesPerPixel * (
+            # takes the pixels from the marker to the center of the image and
+            # multiplies it by the degrees per pixel
+            self.angleToMarker = self.degrees_per_pixel * (
                 centerXMarker - self.frameWidth / 2
             )
 
             """
-            distanceToAR = (knownWidthOfMarker(20cm) * focalLengthOfCamera) / pixelWidthOfMarker
-            focalLength = focal length at 0 degrees horizontal and 0 degrees vertical
-            focalLength30H = focal length at 30 degreees horizontal and 0 degrees vertical
-            focalLength30V = focal length at 30 degrees vertical and 0 degrees horizontal
-            realFocalLength of camera = focalLength 
-                                        + (horizontal angle to marker/30) * (focalLength30H - focalLength)
-                                        + (vertical angle to marker / 30) * (focalLength30V - focalLength)
-            If focalLength30H and focalLength30V both equal focalLength then realFocalLength = focalLength which is good for non huddly cameras
-            Please note that the realFocalLength calculation is an approximation that could be much better if anyone wants to try to come up with something better
+            distanceToAR = 
+                (knownWidthOfMarker(20cm) * focalLengthOfCamera) / pixelWidthOfMarker.
+                
+            focal_length = focal length at 0 degrees horizontal and 0 degrees vertical.
+            focalLength30H = focal len at 30 degrees horizontal and 0 degrees vertical.
+            focalLength30V = focal len at 30 degrees vertical and 0 degrees horizontal.
+            
+            realFocalLength of camera = focal_length 
+                + (horizontal angle to marker/30) * (focalLength30H - focal_length)
+                + (vertical angle to marker / 30) * (focalLength30V - focal_length).
+                
+            If focalLength30H and focalLength30V both equal focal_length,
+            then realFocalLength = focal_length which is good for non huddly cameras.
+            Please note that the realFocalLength calculation is an approximation that
+            could be much better if anyone wants to try to come up with something better
             """
             hAngleToMarker = abs(self.angleToMarker)
             centerYMarker = (
@@ -283,12 +298,12 @@ class ARTracker:
                 + self.corners[self.index1][0][3][1]
             ) / 4
             vAngleToMarker = abs(
-                self.vDegreesPerPixel * (centerYMarker - self.frameHeight / 2)
+                self.vdegrees_per_pixel * (centerYMarker - self.frameHeight / 2)
             )
             realFocalLength = (
-                self.focalLength
-                + (hAngleToMarker / 30) * (self.focalLength30H - self.focalLength)
-                + (vAngleToMarker / 30) * (self.focalLength30V - self.focalLength)
+                self.focal_length
+                + (hAngleToMarker / 30) * (self.focalLength30H - self.focal_length)
+                + (vAngleToMarker / 30) * (self.focalLength30V - self.focal_length)
             )
             widthOfMarker = (
                 (
@@ -317,15 +332,15 @@ class ARTracker:
                 + self.corners[self.index2][0][2][0]
                 + self.corners[self.index2][0][3][0]
             ) / 4
-            self.angleToMarker = self.degreesPerPixel * (
+            self.angleToMarker = self.degrees_per_pixel * (
                 (centerXMarker1 + centerXMarker2) / 2 - self.frameWidth / 2
             )
 
             hAngleToMarker1 = abs(
-                self.vDegreesPerPixel * (centerXMarker1 - self.frameWidth / 2)
+                self.vdegrees_per_pixel * (centerXMarker1 - self.frameWidth / 2)
             )
             hAngleToMarker2 = abs(
-                self.vDegreesPerPixel * (centerXMarker2 - self.frameWidth / 2)
+                self.vdegrees_per_pixel * (centerXMarker2 - self.frameWidth / 2)
             )
             centerYMarker1 = (
                 self.corners[self.index1][0][0][1]
@@ -340,20 +355,20 @@ class ARTracker:
                 + self.corners[self.index2][0][3][1]
             ) / 4
             vAngleToMarker1 = abs(
-                self.vDegreesPerPixel * (centerYMarker1 - self.frameHeight / 2)
+                self.vdegrees_per_pixel * (centerYMarker1 - self.frameHeight / 2)
             )
             vAngleToMarker2 = abs(
-                self.vDegreesPerPixel * (centerYMarker2 - self.frameHeight / 2)
+                self.vdegrees_per_pixel * (centerYMarker2 - self.frameHeight / 2)
             )
             realFocalLength1 = (
-                self.focalLength
-                + (hAngleToMarker1 / 30) * (self.focalLength30H - self.focalLength)
-                + (vAngleToMarker1 / 30) * (self.focalLength30V - self.focalLength)
+                self.focal_length
+                + (hAngleToMarker1 / 30) * (self.focalLength30H - self.focal_length)
+                + (vAngleToMarker1 / 30) * (self.focalLength30V - self.focal_length)
             )
             realFocalLength2 = (
-                self.focalLength
-                + (hAngleToMarker2 / 30) * (self.focalLength30H - self.focalLength)
-                + (vAngleToMarker2 / 30) * (self.focalLength30V - self.focalLength)
+                self.focal_length
+                + (hAngleToMarker2 / 30) * (self.focalLength30H - self.focal_length)
+                + (vAngleToMarker2 / 30) * (self.focalLength30V - self.focal_length)
             )
             widthOfMarker1 = (
                 (
@@ -376,7 +391,8 @@ class ARTracker:
                 )
             ) / 2
 
-            # distanceToAR = (knownWidthOfMarker(20cm) * focalLengthOfCamera) / pixelWidthOfMarker
+            # distanceToAR = (knownWidthOfMarker(20cm) *
+            #       focalLengthOfCamera) / pixelWidthOfMarker
             distanceToMarker1 = (
                 self.knownMarkerWidth * realFocalLength1
             ) / widthOfMarker1
